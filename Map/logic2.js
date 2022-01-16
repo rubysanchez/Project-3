801fea0e1359bfa900a568a88189e56b1652afc2
AmyP93 <89039013+AmyP93@users.noreply.github.com>
2022-01-15 19:18:35 -0600

    update to TX long-lat AP

--- Map/logic2.js
+++ Map/logic2.js
@@ -1,8 +1,9 @@
 // Create a map object.
 var myMap = L.map("map", {
-   center: [37.09, -95.71],
-  zoom: 4
-  
+    center: [31.96, -99.90],
+    // center: [37.09, -90.71],
+    zoom: 6
+
 });
 
 // Add a tile layer.
@@ -12,17 +13,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // save a reference to the selected city
 
-d3.select("#hospitalselect").on("change",change);
+d3.select("#hospitalselect").on("change", change);
 
-function change(){
-  hospital = this.options[this.selectedIndex].value
+function change() {
+    hospital = this.options[this.selectedIndex].value
 
-  infectionObject = data.filter(item => item.name === hospital)
+    infectionObject = data.filter(item => item.name === hospital)
 
-  L.marker([infectionObject.latitude,infectionObject.longitude],{  
+    L.marker([infectionObject.latitude, infectionObject.longitude], {
 
-  }).bindPopup(`${infectionObject.name}<hr>${infectionObject["hai pct"]}`).addTo(myMap);
+    }).bindPopup(`${infectionObject.name}<hr>${infectionObject["hai pct"]}`).addTo(myMap);
 
 
 }
-
