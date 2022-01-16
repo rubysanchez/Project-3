101b8af8a23e640b59beb1921a4983fc0238b7f0
rubysanchez <89670617+rubysanchez@users.noreply.github.com>
2022-01-16 15:34:43 -0600

    JSON data files

---
+++ JSON data/hospitalbottomten.js
@@ -0,0 +1,72 @@
+[
+{
+"denominator": 86244,
+"id": "45069F",
+"name": "Brooke Army Medical Center  (FT Sam Houston)",
+"numerator": 73,
+"pct_infections": "0.08%"
+},
+{
+"denominator": 72859,
+"id": "450023",
+"name": "CITIZENS MEDICAL CENTER",
+"numerator": 53,
+"pct_infections": "0.07%"
+},
+{
+"denominator": 72672,
+"id": "450147",
+"name": "DE TAR HOSPITAL NAVARRO",
+"numerator": 43,
+"pct_infections": "0.06%"
+},
+{
+"denominator": 67728,
+"id": "450128",
+"name": "KNAPP MEDICAL CENTER",
+"numerator": 38,
+"pct_infections": "0.06%"
+},
+{
+"denominator": 489487,
+"id": "450015",
+"name": "PARKLAND HEALTH & HOSPITAL SYSTEM",
+"numerator": 270,
+"pct_infections": "0.06%"
+},
+{
+"denominator": 162414,
+"id": "450723",
+"name": "METHODIST CHARLTON MEDICAL CENTER",
+"numerator": 80,
+"pct_infections": "0.05%"
+},
+{
+"denominator": 595043,
+"id": "450358",
+"name": "HOUSTON METHODIST HOSPITAL",
+"numerator": 277,
+"pct_infections": "0.05%"
+},
+{
+"denominator": 47393,
+"id": "450347",
+"name": "HUNTSVILLE MEMORIAL HOSPITAL",
+"numerator": 26,
+"pct_infections": "0.05%"
+},
+{
+"denominator": 55646,
+"id": "450271",
+"name": "WISE REGIONAL HEALTH SYSTEM",
+"numerator": 27,
+"pct_infections": "0.05%"
+},
+{
+"denominator": 159416,
+"id": "450237",
+"name": "CHRISTUS SANTA ROSA MEDICAL CENTER",
+"numerator": 72,
+"pct_infections": "0.05%"
+}
+]
---
+++ JSON data/hospitaltopten.js
@@ -0,0 +1,72 @@
+[
+{
+"denominator": 40753,
+"id": "450822",
+"name": "MEDICAL CITY LAS COLINAS",
+"numerator": 1,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 40045,
+"id": "450747",
+"name": "PALESTINE REGIONAL MEDICAL CENTER",
+"numerator": 1,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 67862,
+"id": "450688",
+"name": "DALLAS REGIONAL MEDICAL CENTER",
+"numerator": 0,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 42620,
+"id": "450678",
+"name": "BAYLOR SCOTT & WHITE MEDICAL CENTER-WHITE ROCK",
+"numerator": 2,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 37334,
+"id": "450272",
+"name": "CHRISTUS SANTA ROSA HOSPITAL-SAN MARCOS",
+"numerator": 0,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 25269,
+"id": "450072",
+"name": "CHI ST. LUKES' BRAZOSPORT HOSPITAL",
+"numerator": 0,
+"pct_infections": "0.00%"
+},
+{
+"denominator": 100254,
+"id": "670122",
+"name": "HOUSTON METHODIST THE WOODLANDS HOSPITAL",
+"numerator": 6,
+"pct_infections": "0.01%"
+},
+{
+"denominator": 59665,
+"id": "670085",
+"name": "TEXAS HEALTH HARRIS METHODIST HOSPITAL ALLIANCE",
+"numerator": 5,
+"pct_infections": "0.01%"
+},
+{
+"denominator": 30404,
+"id": "670080",
+"name": "SETON MEDICAL CENTER HARKER HEIGHTS",
+"numerator": 4,
+"pct_infections": "0.01%"
+},
+{
+"denominator": 47803,
+"id": "670060",
+"name": "BAYLOR SCOTT AND WHITE MEDICAL CENTER SUNNYVALE",
+"numerator": 3,
+"pct_infections": "0.01%"
+}
+]
